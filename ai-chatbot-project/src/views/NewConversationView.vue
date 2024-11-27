6c610f619aa1bc7cb717c38bba03a7f2b8341a97
Brandon Dalla Rosa <brandondr96@gmail.com>
2024-11-27 00:05:44 -0500

    Existence is pain.

--- ai-chatbot-project/src/views/NewConversationView.vue
+++ ai-chatbot-project/src/views/NewConversationView.vue
@@ -1,19 +1,65 @@
 <template>
-    <div >
-      <h1> chat with google wahoo</h1>
-      <p> {{  props.query }}</p>
-    </div>
-  </template>
-  <script setup>
-import { defineProps } from 'vue';
-const props = defineProps({
-  query: {
-    type: String,
-    required: false
-  }
-});
+  <div >
+    <h1> {{ props.query }} </h1>
+    <p> {{ message }}</p>
+  </div>
+</template>
+<script setup>
+  import { defineProps } from 'vue';
+  const props = defineProps({
+    query: {
+      type: String,
+      required: false
+    }
+  });
 
-console.log(props.query)
+  console.log(props.query)
+</script>
+
+<script>
+  import { marked } from "https://cdn.jsdelivr.net/npm/marked@13.0.3/lib/marked.esm.js";
+  import DOMPurify from "https://cdn.jsdelivr.net/npm/dompurify@3.1.6/dist/purify.es.mjs";
+  export default {
+    data() {
+      return {
+        message: 'Loading...',
+        session: null,
+      };
+    },
+    methods: {
+      async promptModel() {
+        if (!self.ai || !self.ai.languageModel) {
+          this.message = "I'm sorry, your browser does not support cool stuff";
+        }
+        const prompt = props.query;
+        if (!prompt) return;
+        let fullResponse = "";
+        try {
+          if (!session) {
+            await this.updateSession();
+          }
+          const stream = await session.promptStreaming(prompt);
+
+          for await (const chunk of stream) {
+            fullResponse = chunk.trim();
+            p.innerHTML = DOMPurify.sanitize(marked.parse(fullResponse));
+            this.message = fullResponse;
+          }
+        } catch (error) {
+          p.textContent = `Error: ${error.message}`;
+        }
+      },
+      async updateSession() {
+        session = await self.ai.languageModel.create({
+          temperature: 0.5,
+          topK: 40,
+        })
+      }
+    },
+    beforeMount(){
+    	this.promptModel();
+    }
+  };
 </script>
   
   <style>
